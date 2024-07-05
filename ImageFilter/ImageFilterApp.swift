25896884b328ef30a6ce9f4921792291004e72af
Huiping Guo <huiping192@gmail.com>
2024-07-05 23:24:22 +0900

    init

---
+++ ImageFilter/ImageFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFilterApp.swift
+//  ImageFilter
+//
+//  Created by 郭 輝平 on 2024/07/05.
+//
+
+import SwiftUI
+
+@main
+struct ImageFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
